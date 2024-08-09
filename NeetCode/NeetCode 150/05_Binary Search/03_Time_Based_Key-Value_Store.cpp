a48c31d47a89c60b468b406d5a87a947415045a6
Prateek <94495908+Prateek-16@users.noreply.github.com>
2024-08-09 19:38:49 +0530

    Update 03_Time_Based_Key-Value_Store.cpp

--- NeetCode/NeetCode 150/05_Binary Search/03_Time_Based_Key-Value_Store.cpp
+++ NeetCode/NeetCode 150/05_Binary Search/03_Time_Based_Key-Value_Store.cpp
@@ -48,3 +48,48 @@ Get function :
     so if e is not negative,
         return m[key][e].second
 */
+
+
+class TimeMap {
+public:
+    unordered_map<string, vector<pair<int, string>>> m;
+    TimeMap() {}
+
+    void set(string key, string value, int timestamp) {
+        m[key].push_back({timestamp, value});
+    }
+
+    string get(string key, int timestamp) {
+        if (m.find(key) == m.end()) {
+            return "";
+        }
+        int s = 0;
+        int e = m[key].size() - 1;
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+            if (m[key][mid].first < timestamp) {
+                s = mid + 1;
+            } else if (m[key][mid].first > timestamp) {
+                e = mid - 1;
+            } else {
+                return m[key][mid].second;
+            }
+        }
+        // If we reach here means no value of timestamp is a exact match,
+        // so we need to return just smaller value of the time stamp
+        // since e and s are crossed so e will be at the index just smaller that
+        // the target
+        if (e >= 0) {
+            return m[key][e].second;
+        }
+        // if e is negative return null string
+        return "";
+    }
+};
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * TimeMap* obj = new TimeMap();
+ * obj->set(key,value,timestamp);
+ * string param_2 = obj->get(key,timestamp);
+ */
