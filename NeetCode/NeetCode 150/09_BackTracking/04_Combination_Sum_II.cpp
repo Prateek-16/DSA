aa50ab241cf06243f08f08e80ab26b2748d858b2
Prateek <94495908+Prateek-16@users.noreply.github.com>
2024-08-10 00:15:58 +0530

    Update 04_Combination_Sum_II.cpp

--- NeetCode/NeetCode 150/09_BackTracking/04_Combination_Sum_II.cpp
+++ NeetCode/NeetCode 150/09_BackTracking/04_Combination_Sum_II.cpp
@@ -49,3 +49,34 @@ also make sure that when we loop over all next elements : remember that ith inde
 
 
 */
+
+
+class Solution {
+public:
+    void solve( vector<int>&candidates , int target , vector<vector<int>>&ans , vector<int>&v , int index){
+        //base case
+        if(target==0){
+            ans.push_back(v);
+            return;
+        }
+        if(target<0){
+            return;
+        }
+        for( int i=index ; i<candidates.size() ; i++){
+            if(i>index && candidates[i]==candidates[i-1]){
+                continue;
+            }
+            v.push_back(candidates[i]);
+            solve(candidates , target-candidates[i] , ans , v , i+1);
+            v.pop_back();
+        }
+
+    }
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(),candidates.end());
+        vector<vector<int>> ans;
+        vector<int> v;
+        solve( candidates , target , ans , v , 0);
+        return ans;
+    }
+};
