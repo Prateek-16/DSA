d5fef84a07aa919647b8b92423fee7c2338e8764
Prateek <94495908+Prateek-16@users.noreply.github.com>
2024-08-10 23:18:20 +0530

    Update 02_Add_Two_Numbers.cpp

--- NeetCode/NeetCode 150/06_Linked List/02_Add_Two_Numbers.cpp
+++ NeetCode/NeetCode 150/06_Linked List/02_Add_Two_Numbers.cpp
@@ -43,3 +43,43 @@ Steps :
     5. return dummy -> next
 
 */
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* dummy = new ListNode();
+        ListNode* curr = dummy;
+        int carry = 0;
+        while(l1 != NULL || l2 != NULL){
+            int val1 = (l1 != NULL) ? l1->val : 0;
+            int val2 = (l2 != NULL) ? l2->val : 0;
+
+            int sum = val1 + val2 + carry;
+            carry = sum/10;
+            curr->next = new ListNode(sum%10);
+            curr = curr->next;
+            if(l1 != NULL){
+                l1 = l1->next;
+            }
+            if(l2 != NULL){
+                l2 = l2->next;
+            }
+        }
+        if(carry){
+            curr->next = new ListNode(carry);
+        }
+        return dummy->next;
+        
+    }
+};
