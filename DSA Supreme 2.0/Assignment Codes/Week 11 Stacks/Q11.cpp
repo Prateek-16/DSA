3da202d34806e2960958cac6f9d2af548693e745
Prateek <94495908+Prateek-16@users.noreply.github.com>
2024-08-16 07:24:46 +0530

    Update Q11.cpp

--- DSA Supreme 2.0/Assignment Codes/Week 11 Stacks/Q11.cpp
+++ DSA Supreme 2.0/Assignment Codes/Week 11 Stacks/Q11.cpp
@@ -2,6 +2,16 @@
 
 //Car fleet 2
 
+Steps 
+1. Stack will have those cars which may collide
+2. Simple baat hai kisi bhi new car ka time for collision nikalna hai toh 
+sabse pehele stack se woh cars hata do jo usse tej ho speed mai
+3. Ab stack ke top pe jo car hai woh slow hai current car se toh collision time nikalo
+4. Ab bas yeh condition dekhlo ki jitne time mai humari current car slower car se collide kar rhi hai usse pehele slower car kisi aur car se toh collide nhi kar ja rahi
+5. If false then toh current car slow car se collide karegi and time save kar lo and current car ko stack mai dal dena
+6. But agar slow car kisi aur se collide kar jati hai toh slower car ab us car ka part ban jati hai so uska stack mai koi need nhi so pop it, and current car ka collision time slower ke collided car se find karo 
+
+
 class Solution {
 public:
     vector<double> getCollisionTimes(vector<vector<int>>& cars) {
