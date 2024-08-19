e03f50ad8a8140b725ff3d15bebb6513a4d55aa5
Prateek <94495908+Prateek-16@users.noreply.github.com>
2024-08-20 05:20:43 +0530

    Update Q08.cpp

--- DSA Supreme 2.0/Assignment Codes/Week 13 Generic Tree/Q08.cpp
+++ DSA Supreme 2.0/Assignment Codes/Week 13 Generic Tree/Q08.cpp
@@ -36,3 +36,25 @@ public:
         return count;
     }
 };
+
+*******************************************************
+
+class Solution {
+ public:
+  int pathSum(TreeNode* root, int sum) {
+    if (root == nullptr)
+      return 0;
+    return dfs(root, sum) +            //
+           pathSum(root->left, sum) +  //
+           pathSum(root->right, sum);
+  }
+
+ private:
+  int dfs(TreeNode* root, int sum) {
+    if (root == nullptr)
+      return 0;
+    return (sum == root->val) +                //
+           dfs(root->left, sum - root->val) +  //
+           dfs(root->right, sum - root->val);
+  }
+};
